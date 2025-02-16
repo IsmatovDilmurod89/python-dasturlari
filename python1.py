cb72a813b0c1c14180555d3d024efa26d73f07da
Dilmurod <ismatovdilmurod0103@gmail.com>
2025-02-16 15:33:35 +0500

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,101 @@
+#kvadratini hisoblash
+
+# =============================================================================
+# a = int(input("Son kiriting: "))  
+# print(f"{a} ning kvadrati: {a**2}") 
+# =============================================================================
+
+# =============================================================================
+# #toq yoki juftligini aniqlash
+# b = int(input("Son kiriting: "))
+# if b % 2 == 0:
+#     print("juft son")
+# else:
+#     print("toq son")
+# =============================================================================
+
+# =============================================================================
+# #kiritilgan isimni chiqarish
+# ism = str(input("ismingizni kiriting "))
+# print("Salom ", ism)
+# =============================================================================
+
+# =============================================================================
+# #10 gacha bo'lgan raqamlarni chiqarish
+# for i in range(1, 11, 1): print(i)
+# =============================================================================
+
+# =============================================================================
+# #ikki son yig'indisi
+# a = int(input("birinchi sonni kiriting-"))
+# b = int(input("ikkinchi sonni kiriting-"))
+# print(f"{a} + {b} = {a + b}")
+# =============================================================================
+
+
+# =============================================================================
+# # sonning modulini topish
+# a = int(input("birinchi sonni kiriting-"))
+# print(abs(a))
+# =============================================================================
+
+#Berilgan son musbat yoki manfiy ekanligini aniqlash.
+# =============================================================================
+# a = int(input("sonni kiriting:"))
+# if a > 0:
+#     print("musbat")
+# elif a == 0:
+#     print("no'lga teng")
+# else:
+#     print("manfiy")
+# =============================================================================
+
+#Kvadratning yuzasi va perimetrini topish 
+# =============================================================================
+# tomon = int(input("Kvadratning tomon uzunligini kiriting: "))
+# yuzasi = tomon * tomon
+# perimetri = tomon * 4
+# 
+# print(f"Kvadratning yuzasi: {yuzasi}")
+# print(f"Kvadratning perimetri: {perimetri}")
+# =============================================================================
+
+#Berilgan so‘zning uzunligini topish 
+# =============================================================================
+# soz = input("So‘zni kiriting: ")
+# uzunlik = len(soz)
+# 
+# print(f"So‘z uzunligi: {uzunlik} ta belgi")
+# =============================================================================
+
+
+#Sonni 3 ga va 5 ga bo‘linishini tekshirish 
+# =============================================================================
+# a = int(input("Sonni kiriting: "))
+# 
+# if a % 3 == 0 and a % 5 == 0:
+#     print("Bu son 3 ga ham, 5 ga ham bo‘linadi.")
+# else:
+#     print("Bu son 3 ga va 5 ga bo‘linmaydi.")
+# =============================================================================
+
+
+#Foydalanuvchi yoshini so‘rab, nechanchi yilda tug‘ilganini chiqarish.
+# =============================================================================
+# yosh = int(input("yoshingizni kiriting: "))
+# print(f"siz shu yilda tug'ilgansiz- {2025-yosh}-yil")
+# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
